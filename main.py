601b8a321e281f2c52e6e0f8bbfc5e9e622e2aec
basova.ea61 <basova.lisa@mail.ru>
2021-11-19 17:52:09 +0500

    aaaaaaaaaaa

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+
+import random
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Рисование')
+
+    # Метод срабатывает, когда виджету надо
+    # перерисовать свое содержимое,
+    # например, при создании формы
+    def paintEvent(self, event):
+        # Создаем объект QPainter для рисования
+        qp = QPainter()
+        # Начинаем процесс рисования
+        qp.begin(self)
+        self.draw_flag(qp)
+        # Завершаем рисование
+        qp.end()
+
+    def draw_flag(self, qp):
+        # Задаем кисть
+        qp.setBrush(QColor(255, 255, 0))
+        # Рисуем прямоугольник заданной кистью
+        for i in range(2, 15):
+            x, y = random.randint(w, h), random.randint(w, h)
+            r = random.randint(120, 300)
+            qp.drawEllipse(x, y, x + r, y + r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
